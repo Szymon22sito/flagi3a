7bb481ded5a866aaa7c81fdbac4462c5a1d870b2
Szymon22sito <szymon.sitarczukss@wp.pl>
2024-10-09 13:41:41 +0200

    flagi

--- app/src/main/java/com/example/flagi3a/MainActivity.java
+++ app/src/main/java/com/example/flagi3a/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.flagi3a;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private int licznik = 0;
+    private int bledow = 0;
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+    }
+
+    public void zlaodpowiedz(View view) {
+        Toast.makeText(this, "ten kolor jest w fladze Polski", Toast.LENGTH_SHORT).show();
+        bledow++;
+    }
+
+    public void dobraodpowiedz(View view) {
+        view.setVisibility(View.INVISIBLE);
+        licznik++;
+        if(licznik == 4){
+            textView.setText("To jest flaga Polski\n Pomylono się "+bledow+" razy");
+        }
     }
 }
